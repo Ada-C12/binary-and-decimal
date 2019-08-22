ffa829e0c76bbf9def3b33cf84fe2892a4e1da37
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-08-22 12:09:01 -0700

    implemented binary_to_decimal method

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -5,5 +5,15 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  decimal_array = []
+  binary_array.each_with_index do |binary_num, index|
+    power_of = (binary_array.length - index) - 1
+    decimal_array.push(binary_num * (2 ** power_of))
+  end
+  
+  decimal = 0
+  decimal_array.each do |decimal_component|
+    decimal += decimal_component
+  end
+  return decimal
 end
