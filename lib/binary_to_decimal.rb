c39ddb3d821d21bbda041d1bc7f3e8bd6ef5bb6c
Eve Le <nguyenthao.lee@gmail.com>
2019-08-22 14:29:34 -0700

    Implemented binary_to_decimal, added tests and implemented decimal_to_binary

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -5,5 +5,27 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  size = binary_array.length
+  result = 0
+  size.times {|index| result += binary_array[index] * (2**(size - 1 - index))}
+  return result
+end
+
+def decimal_to_binary(decimal_number)
+  if decimal_number == 0
+    return [0]
+  end
+  
+  temp_output = Array.new
+  while decimal_number != 0
+    temp_output << (decimal_number % 2)
+    decimal_number = decimal_number / 2
+  end
+  # copy element in temp_output to result in reverse order
+  size = temp_output.length
+  result = Array.new(size)
+  size.times do |index|
+    result[size - 1 - index] = temp_output[index]
+  end
+  return result
 end
--- test/binary_to_decimal_test.rb
+++ test/binary_to_decimal_test.rb
@@ -6,28 +6,67 @@ describe "binary to decimal" do
   it "From 10011001 to 153" do
     binary_array = [1, 0, 0, 1, 1, 0, 0, 1]
     expected_decimal = 153
-
+    
     binary_to_decimal(binary_array).must_equal expected_decimal
   end
-
+  
   it "From 00001101 to 13" do
     binary_array = [0, 0, 0, 0, 1, 1, 0, 1]
     expected_decimal = 13
-
+    
     binary_to_decimal(binary_array).must_equal expected_decimal
   end
-
+  
   it "From 10000000 to 128" do
     binary_array = [1, 0, 0, 0, 0, 0, 0, 0]
     expected_decimal = 128
-
+    
     binary_to_decimal(binary_array).must_equal expected_decimal
   end
-
+  
   it "From random binary to decimal" do
     binary_array = Array.new(8) { rand(0..1) }
     expected_decimal = binary_array.join.to_s.to_i(2)
-
+    
     binary_to_decimal(binary_array).must_equal expected_decimal
   end
 end
+
+describe "decimal to binary" do
+  it "From 0 decimal to 0 binary" do
+    decimal = 0
+    expected_binary_array = [0]
+    
+    decimal_to_binary(decimal).must_equal expected_binary_array
+  end
+  
+  it "From 153 to 10011001" do
+    decimal = 153
+    expected_binary_array = [1, 0, 0, 1, 1, 0, 0, 1]
+    
+    decimal_to_binary(decimal).must_equal expected_binary_array
+  end
+  
+  it "From 13 to 00001101" do
+    expected_binary_array = [1, 1, 0, 1]
+    decimal = 13
+    
+    decimal_to_binary(decimal).must_equal expected_binary_array
+  end
+  
+  it "From 128 to 10000000" do
+    expected_binary_array = [1, 0, 0, 0, 0, 0, 0, 0]
+    decimal = 128
+    
+    decimal_to_binary(decimal).must_equal expected_binary_array
+  end
+  
+  it "From 255 to 11111111" do
+    expected_binary_array = [1, 1, 1, 1, 1, 1, 1, 1]
+    decimal = 255
+    
+    decimal_to_binary(decimal).must_equal expected_binary_array
+  end
+  
+end
+
