aaf2536e481781696d6ebf0b06c378876e97c5b4
stupendousC <CarolineWuKaplan@gmail.com>
2019-08-22 12:23:14 -0700

    Passed Tests.  Added 3 optional tests

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -1,9 +1,41 @@
-# A method named `binary_to_decimal` that receives as input an array of size 8.
-# The array is randomly filled with 0’s and 1’s.
-# The most significant bit is at index 0.
-# The least significant bit is at index 7.
+
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
+
+
+
+
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  # CHECK if argument is valid
+  if binary_array.class != Array
+    raise ArgumentError, "We need an array"
+  elsif binary_array.length != 8
+    raise ArgumentError, "Array needs to have 8 elements"
+  end
+  
+  # ESTABLISH the model of binary values according to digit placement
+  binary_model = [1]
+  (binary_array.length - 1).times do
+    prev_value = binary_model[0]
+    binary_model.unshift(prev_value * 2)
+  end
+
+  # INITIALIZE answer variable
+  answer = 0
+
+  # back to binary_array
+  binary_array.length.times do |index|
+    element = binary_array[index]
+
+    # check if element is valid
+    unless [0,1].include? element
+      raise ArgumentError, "#{element} must be 0 or 1"
+
+    else 
+      # start calculation
+      value = element * binary_model[index]
+      answer += value
+    end
+  end
+  return answer
 end
--- test/binary_to_decimal_test.rb
+++ test/binary_to_decimal_test.rb
@@ -6,28 +6,51 @@ describe "binary to decimal" do
   it "From 10011001 to 153" do
     binary_array = [1, 0, 0, 1, 1, 0, 0, 1]
     expected_decimal = 153
-
+    
     binary_to_decimal(binary_array).must_equal expected_decimal
   end
-
+  
   it "From 00001101 to 13" do
     binary_array = [0, 0, 0, 0, 1, 1, 0, 1]
     expected_decimal = 13
-
+    
     binary_to_decimal(binary_array).must_equal expected_decimal
   end
-
+  
   it "From 10000000 to 128" do
     binary_array = [1, 0, 0, 0, 0, 0, 0, 0]
     expected_decimal = 128
-
+    
     binary_to_decimal(binary_array).must_equal expected_decimal
   end
-
+  
   it "From random binary to decimal" do
     binary_array = Array.new(8) { rand(0..1) }
     expected_decimal = binary_array.join.to_s.to_i(2)
-
+    
     binary_to_decimal(binary_array).must_equal expected_decimal
+  end
+end
+
+describe "OPTIONAL: Test against invalid arguments" do
+  it "Case: input is not an array" do
+    sample = "1 0 0 1 0 0 1 0"
+    expect {
+      binary_to_decimal(sample)
+    }.must_raise ArgumentError
+  end
+  
+  it "Case: input does not have 8 elements" do
+    sample = [0]
+    expect {
+      binary_to_decimal(sample)
+    }.must_raise ArgumentError
+  end
+  
+  it "Case: input has 8 strings inside" do
+    sample = %w[1 0 1 0 0 1 0 1]
+    expect {
+      binary_to_decimal(sample)
+    }.must_raise ArgumentError
   end
 end
