ecb8759f79ef1e1cbe67a7672f84a03fb2f78961
eaball35 <eaball35@gmail.com>
2019-08-22 22:06:10 -0700

    added decimal to binary

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -4,6 +4,57 @@
 # The least significant bit is at index 7.
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
+
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  raise ArgumentError.new "nil binary inputed" if binary_array == nil
+  
+  values = []
+  arraylength = binary_array.length
+  binary_array.each do |bit|
+    arraylength -= 1
+    value = bit * (2 ** arraylength)
+    values << value
+  end
+  
+  sum = 0
+  values.each do |value|
+    sum += value
+  end
+
+  return sum
 end
+
+    
+def reverse(array)
+  reversed = []
+  until array.empty? do
+      reversed << array.pop
+  end
+  return reversed
+end
+
+
+def decimal_to_binary(decimal)
+  raise ArgumentError.new "nil binary inputed" if decimal == nil
+
+    remainders = []
+    value = decimal
+    until value == 0
+      remainder = value % 2
+      remainders << remainder
+      value = (value / 2).to_i
+    end
+    
+    result = []
+    until remainders.empty? do
+        result << remainders.pop
+    end
+
+    return result
+end
+
+
+
+
+
+
