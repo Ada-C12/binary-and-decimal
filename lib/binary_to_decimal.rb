155d43ca58e0ac0f02c17986c5fb794395caa036
Janice Huang <janicehuang@gmail.com>
2019-08-22 20:05:46 -0700

    resolve warnings: get method out of method, fix parameter spacing

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -1,25 +1,26 @@
 def binary_to_decimal(binary_array)
   total = 0
   index = binary_array.length - 1
-  power_of_two = 0
+  exp = 0
   binary_array.each do |digit|
-    total += binary_array[index] * 2 ** (power_of_two)
+    total += binary_array[index] * (2 ** exp)
     index -= 1
-    power_of_two += 1
+    exp += 1
   end
   return total
 end
 
-def decimal_to_binary(int)
-  int = int.to_i
-  def get_exp (int)
-    exp = 0
-    until (2 ** exp * 2) > int
-      exp += 1
-    end
-    return exp
-  end
 
+def get_exp(int)
+  int.to_i
+  exp = 0
+  until (2 ** exp * 2) > int
+    exp += 1
+  end
+  return exp
+end
+
+def decimal_to_binary(int)
   answer = 0
   until int == 0
     exp = get_exp(int)
