70c0c7c850c3f5b242e7aeefdd351c7540fec2ba
Janice Huang <janicehuang@gmail.com>
2019-08-22 18:12:57 -0700

    write 3 tests for decimal-to-binary

--- test/binary_to_decimal_test.rb
+++ test/binary_to_decimal_test.rb
@@ -31,3 +31,26 @@ describe "binary to decimal" do
     binary_to_decimal(binary_array).must_equal expected_decimal
   end
 end
+
+describe "decimal to binary" do
+  it "from 153 to 10011001" do
+    int = 153
+    expected_binary = 10011001
+
+    decimal_to_binary(int).must_equal expected_binary
+  end
+
+  it "from 13 to 1101" do
+    int = 13
+    expected_binary = 1101
+
+    decimal_to_binary(int).must_equal expected_binary
+  end
+
+  it "from 128 to 10000000" do
+    int = 128
+    expected_binary = 10000000
+
+    decimal_to_binary(int).must_equal expected_binary
+  end
+end
