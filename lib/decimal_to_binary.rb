77cdf9b3f95ed4c2b15d516588c9221c9816244e
Dianna Tingg <dtingg@gmail.com>
2019-08-24 09:20:59 -0700

    implemented my own version of reversing an array

--- lib/decimal_to_binary.rb
+++ lib/decimal_to_binary.rb
@@ -1,19 +1,26 @@
 def decimal_to_binary(decimal_number)
-  reverse_binary_array = []
+  reverse_array = []
   number = decimal_number
   
   until number == 0    
     remainder = number % 2
     
     if remainder == 0
-      reverse_binary_array.push(0)
+      reverse_array.push(0)
     else
-      reverse_binary_array.push(1)
+      reverse_array.push(1)
     end
     
     number /= 2   
   end
   
-  return reverse_binary_array.reverse
+  binary_array = []
+  i = -1
   
+  while i >= -reverse_array.length
+    binary_array.push(reverse_array[i])
+    i -= 1
+  end
+  
+  return binary_array
 end
