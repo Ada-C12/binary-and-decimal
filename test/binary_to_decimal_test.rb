1f414cae7e60c6cc68baeed517656bae3454f2be
Julia Bouvier <juliabouv@gmail.com>
2019-08-24 12:42:41 -0700

    Implemented decimal_to_binary method tests

--- test/binary_to_decimal_test.rb
+++ test/binary_to_decimal_test.rb
@@ -33,3 +33,26 @@ describe "binary to decimal" do
     binary_to_decimal(binary_array).must_equal expected_decimal
   end
 end
+
+describe "decimal to binary" do
+  it "From 153 to 10011001" do
+    decimal = 153
+    expected_binary_array = [1, 0, 0, 1, 1, 0, 0, 1]
+    
+    decimal_to_binary(decimal).must_equal expected_binary_array
+  end
+  
+  it "From 13 to 00001101" do
+    decimal = 13
+    expected_binary_array = [1, 1, 0, 1]
+    
+    decimal_to_binary(decimal).must_equal expected_binary_array
+  end
+  
+  it "From 128 to 10000000" do
+    decimal = 128
+    expected_binary_array = [1, 0, 0, 0, 0, 0, 0, 0]
+    
+    decimal_to_binary(decimal).must_equal expected_binary_array
+  end
+end
