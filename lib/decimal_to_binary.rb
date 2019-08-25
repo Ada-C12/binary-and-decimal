2df807dbf0b05af5b30bc487e175999adae9786d
Emily Vomacka <emilyvomacka@gmail.com>
2019-08-24 22:27:05 -0700

    fixed decimal_to_binary method to handle cases where 2 ** power equals decimal exactly

--- lib/decimal_to_binary.rb
+++ lib/decimal_to_binary.rb
@@ -2,12 +2,12 @@ require 'pry'
 
 def decimal_to_binary(decimal)
   power = 0
-  until (2 ** power) > decimal
+  while (2 ** power) <= decimal
     power += 1
   end 
   power -= 1 
   binary_array = []
-  while power >= 0
+  while power >= 0 
     if decimal / (2 ** power) > 0
       binary_array << 1
       decimal = decimal % (2 ** power)
