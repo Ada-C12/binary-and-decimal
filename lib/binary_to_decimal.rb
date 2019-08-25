e1cf72aafb5c2f5d891ae522b37329627a7d92d0
Georgina Sanchez Buenrostro <cpgeorgina.sanchez@gmail.com>
2019-08-25 15:48:12 -0700

    binary to decimal

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -4,6 +4,24 @@
 # The least significant bit is at index 7.
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
+
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+
+  length = binary_array.length
+  counter = 0
+  decimal_array = []
+  length.times do 
+    decimal_array << 2 ** counter
+    counter += 1
+  end
+
+  decimal_number = 0
+  index = -1
+  binary_array.each do |element|
+    decimal_number += element*decimal_array[index]
+    index -=1
+  end
+
+  return decimal_number
+
 end
