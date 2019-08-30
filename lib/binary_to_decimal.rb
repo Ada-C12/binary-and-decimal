45d17a6df1e300467a8fda6f04c6634363870f0e
Brianna Kemp <kemp.bri@gmail.com>
2019-08-29 23:25:27 -0700

    Assignment Complete

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -1,9 +1,11 @@
-# A method named `binary_to_decimal` that receives as input an array of size 8.
-# The array is randomly filled with 0’s and 1’s.
-# The most significant bit is at index 0.
-# The least significant bit is at index 7.
-# Calculate  and return the decimal value for this binary number using
-# the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  length = binary_array.length - 1
+  decimal_value = 0
+  
+  binary_array.each do |binary_value|
+    temp_value = (binary_value * (2 ** length))
+    decimal_value += temp_value
+    length -= 1
+  end
+  return decimal_value
 end
