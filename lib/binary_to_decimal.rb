04133408b1d7dd1d4e8dfd00da2f96ef6272a44b
Farah Davoodi <farahdavoodi@gmail.com>
2019-09-01 08:48:13 -0700

    Created program to convert binary number to decimal

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -1,3 +1,5 @@
+# Author: Farah Davoodi (Branches)
+
 # A method named `binary_to_decimal` that receives as input an array of size 8.
 # The array is randomly filled with 0’s and 1’s.
 # The most significant bit is at index 0.
@@ -5,5 +7,13 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  total = 0
+  i = binary_array.length
+  
+  binary_array.each do |digit|
+    total += digit*(2**(i-1))
+    i -= 1
+  end
+  
+  return total
 end
