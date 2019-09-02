ec6f4d590406304b93b7a734fcf3b0a254e51cfb
Natalie Tapias <nataliemtapias@gmail.com>
2019-09-02 08:25:25 -0700

    Made a few final edits.

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -2,7 +2,6 @@
 def binary_to_decimal(binary_array)
   decimal = 0
   exponent = 7
-  
   binary_array.each do |digit|
     decimal += (digit * (2**exponent)) 
     exponent -= 1
@@ -12,7 +11,6 @@ end
 
 
 def decimal_to_binary(num)
-  
   binary_number = (num % 2).to_s
   num /= 2
   until num == 0
@@ -22,5 +20,8 @@ def decimal_to_binary(num)
   end
   binary_array = binary_number.split(//)
   return binary_array.map! {|digit| digit = digit.to_i}
+  
+
+  
 end
 
