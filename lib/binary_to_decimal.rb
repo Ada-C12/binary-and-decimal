e5a128ad4b405ffd6a26197329ee15a2877a97c8
Rinostar <codingrinostar@gmail.com>
2019-10-12 15:17:17 -0700

    updated naming conviention (feedback)

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -2,25 +2,22 @@
 # The array is randomly filled with 0’s and 1’s.
 # The most significant bit is at index 0.
 # The least significant bit is at index 7.
-# Calculate  and return the decimal value for this binary number using
+# Calculate and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  n = binary_array.length - 1
-  m = 0
-  r = 0
+  bit = binary_array.length - 1
+  exponent = 0
+  decimal_result = 0
 
   if binary_array.class != Array
-    # Not really sure if we need to implement this expection
-    # Or what situation this exception applies...
-    # Assuming and implementing as input validation
-    raise NotImplementedError
+    raise ArgumentError
   else
-    until n < 0 
-      r += binary_array[n] * (2 ** m)
-      n -= 1
-      m += 1
+    until bit < 0 
+      decimal_result += binary_array[bit] * (2 ** exponent)
+      bit -= 1
+      exponent += 1
     end 
   end
   
-  return r
+  return decimal_result
 end
